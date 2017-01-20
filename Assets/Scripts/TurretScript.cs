d5662a432772ec4dbfcd8c3e48436dc40c6f9b7e
Sinisa Laptop <sinisahamburg@hotmail.de>
2017-01-21 00:23:04 +0100

    [ADDED]Standard Turret, looks at enemy

---
+++ Assets/Scripts/TurretScript.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretScript : MonoBehaviour {
+
+    public Transform target;
+    public float range;
+
+    public string enemyTag = "Enemy";
+
+	// Use this for initialization
+	void Start ()
+    {
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (target == null)
+            return;
+
+        Vector2 dir = target.position - transform.position;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+	}
+
+    void UpdateTarget()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+
+        foreach (GameObject enemy in enemies)
+        {
+            float distanceToEnemy = Vector2.Distance(transform.position, enemy.transform.position);
+            if(distanceToEnemy < shortestDistance)
+            {
+                shortestDistance = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
+        }
+
+        if(nearestEnemy != null && shortestDistance <= range)
+        {
+            target = nearestEnemy.transform;
+        }
+        else
+        {
+            target = null;
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+}
