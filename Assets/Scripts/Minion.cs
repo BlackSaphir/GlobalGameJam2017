a8d2a7b2dfb26fd66dff0fa80cb96d03723193a6
Romanus <romanusklatt@gmx.de>
2017-01-21 01:37:09 +0100

    [Added] Minion script

--- Assets/Scripts/Minion.cs
+++ Assets/Scripts/Minion.cs
@@ -2,15 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Minion : MonoBehaviour {
+public class Minion : MonoBehaviour
+{
+    public int speed;
+    public float life;
+    public int defense;
+    public bool alive;
+    public int money;
+    public GameObject[] Waypoints;
+    public int index = 0;
+    // Use this for initialization
+    void Start()
+    {
+        this.tag = "Enemy";
+        alive = true;
+    }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        MoveTo();
+        if (life<=0)
+        {
+            alive = false;
+            Destroy(this);
+        }
+
+    }
+
+    public void MoveTo()
+    {
+        if (transform.position == Waypoints[index].transform.position && index < Waypoints.Length - 1)
+        {
+            index++;
+        }
+        transform.position = Vector2.MoveTowards(transform.position, Waypoints[index].transform.position,1f);
+    }
 }
