58e052b9a13fef06756ea22e60504380d9e5970f
Carsten <carsten.dollack@yahoo.de>
2017-01-21 13:08:09 +0100

    [ADDED]HUD

---
+++ Assets/Scripts/HUD/Button_One.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Button_One : MonoBehaviour
+{
+    bool clickable;
+    public Button ButtonOne;
+
+
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        clickable = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ClickButton();
+    }
+
+    public void ClickButton()
+    {
+        if (GameObject.Find("Gold").GetComponent<Gold>().Money >= 100 && ButtonOne.interactable == false)
+        {
+            ButtonOne.interactable = true;
+        }
+        else
+        {
+            ButtonOne.interactable = false;
+        }
+    }
+}
---
+++ Assets/Scripts/HUD/NextWave.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class NextWave : MonoBehaviour
+{
+    private int waveCounter;
+    public Text DisplayWave;
+
+    // Use this for initialization
+    void Start()
+    {
+        waveCounter = 0;
+        InvokeRepeating("WaveCounter", 0.0f, 45.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void WaveCounter()
+    {
+        waveCounter += 1;
+        DisplayWave.text = "Wave " + waveCounter.ToString();
+    }
+}
