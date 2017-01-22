50582eb5b1a41df4713b3674fb7107ed97461afb
Waldemar Just <Waldemarjust@gmx.de>
2017-01-22 14:53:29 +0100

    [Updated] spawns

--- Assets/Scripts/DamsScript.cs
+++ Assets/Scripts/DamsScript.cs
@@ -6,11 +6,13 @@ public class DamsScript : MonoBehaviour
 {
     public float HealthTime = 100.0f;
     public bool damBroken;
-
+    public GameManager gm;
+    public int waves;
     public Gif gif;
 
     void Start()
-    {        
+    {
+        gm = GameObject.FindObjectOfType<GameManager>();
         damBroken = false;
     }
 
@@ -18,7 +20,7 @@ public class DamsScript : MonoBehaviour
     void Update()
     {
         HealthTime -= Time.deltaTime;
-        if (HealthTime <= 0)
+        if (gm.Waves > waves)
         {
             gif.kacke = false;
             Destroy(gameObject);
