610ce85879c039bc088365ce763ce79b20d208a2
Waldemar Just <Waldemarjust@gmx.de>
2017-01-22 14:42:26 +0100

    [Updated] gamemanager minionspawn

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -30,6 +30,7 @@ public class GameManager : MonoBehaviour
     public float SpawnTimer;
 
     private GameObject MinionPre;
+    public float waveTimer;
     float Lane1Timer;
     float Lane2Timer;
     float Lane3Timer;
@@ -42,98 +43,132 @@ public class GameManager : MonoBehaviour
     // Use this for initialization
     void Start()
     {
+        Waves = 1;
         Lane = GameObject.Find("Lane1");
         MinionLane1Count = 0;
         MinionLane2Count = 0;
         MinionLane3Count = 0;
         MinionLane4Count = 0;
         WaveLength = 10;
+        waveTimer = 0;
         Lane1Timer = 0;
         Lane2Timer = 0;
         Lane3Timer = 0;
         Lane4Timer = 0;
         MinionPre = MinionFish;
         PlayTime = 0.0f;
-        Minion = gameObject.GetComponent<Minion>();
+        //Minion = gameObject.GetComponent<Minion>();
+        //SpawnMinion(MinionJellyFish, "Lane1");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (damGrid1.dam == null)
+        //if (damGrid1.dam == null)
+        //{
+        //    WaveSpawnLane2();
+        //    Lane2Timer += Time.deltaTime;
+        //}
+
+        //if (damGrid2.dam == null)
+        //{
+        //    WaveSpawnLane3();
+        //    Lane3Timer += Time.deltaTime;
+        //}
+
+        //if (damGrid3.dam == null)
+        //{
+        //    WaveSpawnLane4();
+        //    Lane4Timer += Time.deltaTime;
+        //}
+
+        //WaveSpawnLane1();
+
+
+        SpawnMinion(MinionFish, "Lane1", 5 * Waves);
+
+        if (Waves >= 4)
         {
-            WaveSpawnLane2();
-            Lane2Timer += Time.deltaTime;
+            SpawnMinion(MinionJellyFish, "Lane2", 5 *( Waves - 4));
         }
 
-        if (damGrid2.dam == null)
+        if (Waves >= 8)
         {
-            WaveSpawnLane3();
-            Lane3Timer += Time.deltaTime;
+            SpawnMinion(MinionTurtle, "Lane4", 5 * (Waves - 8));
         }
 
-        if (damGrid3.dam == null)
+        if (Waves >= 20)
         {
-            WaveSpawnLane4();
-            Lane4Timer += Time.deltaTime;
+            SpawnMinion(MinionLaserShark, "Lane3", 5 * (Waves - 20));
         }
 
-        WaveSpawnLane1();
+
+
+
         Lane1Timer += Time.deltaTime;
+        Lane2Timer += Time.deltaTime;
 
         PlayTime += Time.deltaTime;
         PlayTime = Mathf.Round(PlayTime * 100.0f) / 100.0f;
     }
 
-    void WaveSpawnLane1()
+    void SpawnMinion(GameObject minion, string lane, int amount)
     {
-        MinionPre = MinionFish;
-        Lane = GameObject.Find("Lane1");
-        if (Lane1Timer > 1.5f && MinionLane1Count < WaveLength)
-        {
-            GameObject temp = (GameObject)Instantiate(MinionPre, Lane.transform.position, Lane.transform.rotation);
-            temp.GetComponent<Minion>().Lane = GameObject.Find("Lane1");
-            MinionLane1Count++;
-            Lane1Timer = 0;
-        }
-    }
+        Lane = GameObject.Find(lane);
 
-    void WaveSpawnLane2()
-    {
-        MinionPre = MinionTurtle;
-        Lane = GameObject.Find("Lane2");
-        if (Lane2Timer > 2.0f && MinionLane2Count < WaveLength)
+        if (lane == "Lane1")
         {
-            GameObject temp = (GameObject)Instantiate(MinionPre, Lane.transform.position, Lane.transform.rotation);
-            temp.GetComponent<Minion>().Lane = GameObject.Find("Lane2");
-            MinionLane2Count++;
-            Lane2Timer = 0;
+            if (Lane1Timer > 1.5f && MinionLane1Count < amount)
+            {
+                GameObject temp = (GameObject)Instantiate(minion, Lane.transform.position, Lane.transform.rotation);
+                temp.GetComponent<Minion>().Lane = GameObject.Find(lane);
+                MinionLane1Count++;
+                Lane1Timer = 0;
+            }
         }
-    }
 
-    void WaveSpawnLane3()
-    {
-        MinionPre = MinionJellyFish;
-        Lane = GameObject.Find("Lane3");
-        if (Lane3Timer > 1.5f && MinionLane3Count < WaveLength)
+        if (lane == "Lane2")
         {
-            GameObject temp = (GameObject)Instantiate(MinionPre, Lane.transform.position, Lane.transform.rotation);
-            temp.GetComponent<Minion>().Lane = GameObject.Find("Lane3");
-            MinionLane3Count++;
-            Lane3Timer = 0;
+            if (Lane2Timer > 1.5f && MinionLane2Count < amount)
+            {
+                GameObject temp = (GameObject)Instantiate(minion, Lane.transform.position, Lane.transform.rotation);
+                temp.GetComponent<Minion>().Lane = GameObject.Find(lane);
+                MinionLane2Count++;
+                Lane2Timer = 0;
+            }
         }
-    }
 
-    void WaveSpawnLane4()
-    {
-        MinionPre = MinionLaserShark;
-        Lane = GameObject.Find("Lane4");
-        if (Lane4Timer > 1.5f && MinionLane4Count < WaveLength)
+
+        if (lane == "Lane3")
         {
-            GameObject temp = (GameObject)Instantiate(MinionPre, Lane.transform.position, Lane.transform.rotation);
-            temp.GetComponent<Minion>().Lane = GameObject.Find("Lane4");
-            MinionLane4Count++;
-            Lane4Timer = 0;
+            if (Lane3Timer > 1.5f && MinionLane3Count < amount)
+            {
+                GameObject temp = (GameObject)Instantiate(minion, Lane.transform.position, Lane.transform.rotation);
+                temp.GetComponent<Minion>().Lane = GameObject.Find(lane);
+                MinionLane3Count++;
+                Lane3Timer = 0;
+            }
+        }
+        if (lane == "Lane4")
+        {
+            if (Lane4Timer > 1.5f && MinionLane4Count < amount)
+            {
+                GameObject temp = (GameObject)Instantiate(minion, Lane.transform.position, Lane.transform.rotation);
+                temp.GetComponent<Minion>().Lane = GameObject.Find(lane);
+                MinionLane4Count++;
+                Lane4Timer = 0;
+            }
+        }
+
+        if (MinionLane1Count >= 5 * Waves)
+        {
+            waveTimer += Time.deltaTime;
+            if (waveTimer >= 10f)
+            {
+                Waves++;
+                MinionLane1Count = 0;
+                waveTimer = 0f;
+            }
         }
     }
 
